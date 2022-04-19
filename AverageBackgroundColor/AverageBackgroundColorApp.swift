321247d35ace9cdcf696f9b0a020a828cc6bede1
maximiliano morales <maximilianocristian.morales@bbva.com>
2022-04-19 19:42:53 -0300

    Initial Commit

---
+++ AverageBackgroundColor/AverageBackgroundColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AverageBackgroundColorApp.swift
+//  AverageBackgroundColor
+//
+//  Created by Maximiliano Morales on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AverageBackgroundColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AverageBackgroundColor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AverageBackgroundColor
+//
+//  Created by Maximiliano Morales on 19/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
